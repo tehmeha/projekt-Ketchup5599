b8492504fb3709d1f767722f77af92e2ef5b8c55
Ketchup5599 <filip.cvetko95@gmail.com>
2017-11-14 15:32:49 +0100

    unos novih korisnika

--- program.cpp
+++ program.cpp
@@ -4,15 +4,30 @@ using namespace std;
 
 int main ()
 {
-    int broj_racuna [10]={256325,666,6541,849852,416,165,1236,59,185,78};
-    string ime_prezime [10]={"Mato Bajic", "Fran Benkus", "Antonio Rimac", "Ivan Pintaric", "Mali Cuks", "Ivan Buzuk", "Fran hrzic", "Leonardo Peloza", "Antonio Pajcek", "Cofi Gljiva" };
-    double stanje [10]={69.69, 250.00, 569.26, 48.13, 486.23, 78.5, 814.59, -999.99, -48.45, 59.89};
+    int broj_racuna [50]={256325,666,6541,849852,416,165,1236,59,185,78};
+    string ime_prezime [50]={"Mato Bajic", "benkus pao", "Jakov Cretnik", "Ivan Pintaric", "Mali Cuks", "Ivan Buzuk", "Fran hrzic", "Leonardo Peloza", "Antonio Pajcek", "Cofi Gljiva" };
+    int broj_korisnika = 50;
+    double stanje[50]={69.69, 250.00, 3456.5, 48.13, 486.23, 78.5, 814.59, -999.99, -48.45, 59.89};
 
-    for (int i=0; i<10; i++)
+    for (int i=0; i<broj_korisnika; i++)
     {
-        cout<<broj_racuna[i]<<"\t" << ime_prezime[i]<<"\t"<< stanje[i];
+        cout<<broj_racuna[i]<<"\t" << ime_prezime[i]<<"\t"<< stanje[i] << endl;
     }
 
+int novi_broj_racuna;
+string ime_prezime;
+double stanje;
+cout << "Unesite novi broj racuna ";
+cin >> novi_broj_racuna;
+cout << "Unesite ime i prezime korisnika ";
+cin >> ;
+cout << "Unesite stanje racuna ";
+cin >> ;
+
+broj_racuna[broj_korisnika] = novi_broj_racuna;
+ime_prezime[broj_korisnika] = ;
+[broj_korisnika] =
+broj_korisnika++;
 
     return 0;
 }
